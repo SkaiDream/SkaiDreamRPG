267bfed4264852a30314171c81f141d3036a562e
SkaiDream <makedream29@gmail.com>
2016-08-26 15:11:01 +0900

    initial released

---
+++ src/main/java/com/gmail/makedream29/SkaiDreamRPG/server/SkaiDreamRPG.java
@@ -0,0 +1,21 @@
+package com.gmail.makedream29.SkaiDreamRPG.server;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SkaiDreamRPG extends JavaPlugin implements Listener
+{
+	@Override
+	public void onEnable()
+	{
+		// TODO Auto-generated method stub
+		super.onEnable();
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		// TODO Auto-generated method stub
+		super.onDisable();
+	}
+}
